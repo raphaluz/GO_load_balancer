09571d7ce724059a21a2baec61934731d44d848e
Raphael Luz <59450945+raphaluz@users.noreply.github.com>
2024-06-08 06:57:53 -0400

    feat: implement core function to main

--- main.go
+++ main.go
@@ -1,26 +1,56 @@
 package main
 
 import (
-	"fmt",
-	"go_load_balancer/config"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
 )
 
-func loadConfig(file string) (Config, error) {
-	var config Config
-
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return config, err
-	}
-
-	err = json.Unmarshal(bytes, &config)
-	if err != nil {
-		return config, err
-	}
-
-	return config, nil
-}
-
 func main() {
-	fmt.Println("Hello, World!")
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading configuration: %s", err.Error())
+	}
+
+	healthCheckInterval, err := time.ParseDuration(config.HealthCheckInterval)
+	if err != nil {
+		log.Fatalf("Invalid health check interval: %s", err.Error())
+	}
+
+	var servers []*Server
+	for _, serverUrl := range config.Servers {
+		u, _ := url.Parse(serverUrl)
+		servers = append(servers, &Server{URL: u})
+	}
+
+	for _, server := range servers {
+		go func(s *Server) {
+			for range time.Tick(healthCheckInterval) {
+				res, err := http.Get(s.URL.String())
+				if err != nil || res.StatusCode >= 500 {
+					s.Healthy = false
+				} else {
+					s.Healthy = true
+				}
+			}
+		}(server)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		server := nextServerLeastActive(servers)
+		server.Mutex.Lock()
+		server.ActiveConnections++
+		server.Mutex.Unlock()
+		server.Proxy().ServeHTTP(w, r)
+		server.Mutex.Lock()
+		server.ActiveConnections--
+		server.Mutex.Unlock()
+	})
+
+	log.Println("Starting server on port", config.ListenPort)
+	err = http.ListenAndServe(config.ListenPort, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %s\n", err)
+	}
 }
