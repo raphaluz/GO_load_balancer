5e69ccb4d2eca1b72198d9cf9a2f25cf7b1f6257
Raphael Luz <59450945+raphaluz@users.noreply.github.com>
2024-06-08 06:55:25 -0400

    feat: add loadConfig func

--- config.go
+++ config.go
@@ -1,7 +1,28 @@
-package config
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
 
 type Config struct {
 	HealthCheckInterval string   `json:"healthCheckInterval"`
 	Servers             []string `json:"servers"`
 	ListenPort          string   `json:"listenPort"`
+}
+
+func loadConfig(file string) (Config, error) {
+	var config Config
+
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
